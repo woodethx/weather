f0700d42fa1a2f8446c8aa3a661b60bc8ed42c5d
Ethan Wood <woodethx@gmail.com>
2025-05-29 11:48:41 -0500

    Created getWeather module that takes in a location and returns the valid weather data as json

---
+++ src/getWeather.js
@@ -0,0 +1,23 @@
+//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY
+
+export default async function getWeatherData(location){
+    const request = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+location+'?key=V4DL2LQWY6C7JZRDVDVET9DN4');
+    const raw =  await request.json();
+    const prettyWeather = {
+        address: raw.resolvedAddress,
+        currentSky: raw.currentConditions.conditions,
+        currentTemp: raw.currentConditions.temp,
+        days: []
+    }
+    for (let i = 0; i < 7; i++) {
+        const r = raw.days[i];
+        prettyWeather.days[i] = {
+            date: r.datetime,
+            sky: r.conditions,
+            low: r.tempmin,
+            high: r.tempmax,
+        }
+    }
+    console.log(raw);
+    return prettyWeather;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./styles.css";
+import getWeatherData from "./getWeather";
+
+const weatherData = await getWeatherData("collegestation");
+console.log(weatherData);
